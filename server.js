92987ef72326446ff0866f6549c5ebab6b685f02
Ahsan Irfan <ahsan.irfan@gmail.com>
2022-06-22 17:55:46 -0400

    More experiments.

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const fs = require("fs");
 
 // Requiring the Express JS Package to create servers
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
 // Requiring the OpenAI Package to experiment with GPT3
 const { Configuration, OpenAIApi} = require("openai");
@@ -15,11 +15,11 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-// Create fine-tune file: file-a49PpBesM9WjB1ETglNRiVui
+// Create fine-tune file
 
 // openai
 //     .createFineTune({
-//         training_file: "file-a49PpBesM9WjB1ETglNRiVui",
+//         training_file: "file-0vVFcJML44g8ytipDAhx7IHe",
 //         model: "davinci"
 //     })
 //     .then((response) => {
@@ -29,7 +29,9 @@ const openai = new OpenAIApi(configuration);
 // openai
 //     .listFineTunes()
 //     .then((response) => {
-//         console.log(response.data.data)
+//         // console.log(response.data.data);
+//         console.log(response.data.data[6].status);
+//         console.log(response.data.data[6].fine_tuned_model);
 //     });
 
 // openai
@@ -54,15 +56,26 @@ const openai = new OpenAIApi(configuration);
 //         console.log(response.data.choices[0].text);
 //     });
 
+// openai
+//     .createCompletion({
+//         model: 'davinci:ft-personal-2022-06-22-21-09-27',
+//         prompt: 'Write one sentence about personal development.',
+//         max_tokens: 100,
+//         temperature: 1
+//     })
+//     .then((response) => {
+//         console.log(response.data.choices[0].text);
+//     });
+
 // Upload training data
 
-openai
-    .createFile(
-    fs.createReadStream("data.jsonl"), "fine-tune"
-)
-    .then((response) => {
-        console.log(response);
-    });
+// openai
+//     .createFile(
+//     fs.createReadStream("data.jsonl"), "fine-tune"
+// )
+//     .then((response) => {
+//         console.log(response);
+//     });
 
 
 // Requiring the read-excel-file/node package
@@ -77,16 +90,16 @@ openai
         
 //     });
 
-// openai
-//     .createCompletion({
-//         model: "text-davinci-002",
-//         prompt: "Create 24 tweets on the topic of self-discipline.",
-//         max_tokens: 4000,
-//         temperature: 1
-//     })
-//     .then((response) => {
-//         console.log(response.data.choices[0].text);
-//     });
+openai
+    .createCompletion({
+        model: "text-davinci-002",
+        prompt: "Rewrite this sentence in the active voice: Meditation is practiced to focus attention on a single point which develops a more profound level of inner peace.",
+        max_tokens: 4000,
+        temperature: 1
+    })
+    .then((response) => {
+        console.log(response.data.choices[0].text);
+    });
   
 
 // app.get('/', (req, res) => {
@@ -94,6 +107,6 @@ openai
 //     console.log('Responded to GET request!');
 // });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}!`);
-});
+// app.listen(process.env.PORT, () => {
+//     console.log(`Listening on port ${process.env.PORT}!`);
+// });
